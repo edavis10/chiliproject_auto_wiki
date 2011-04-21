fac13aa98b23d5f6541a2a35121c21f52179d256
Eric Davis <edavis@littlestreamsoftware.com>
2011-04-21 15:01:00 -0700

    Refactor: extract method

--- lib/chiliproject_auto_wiki/patches/enabled_module_patch.rb
+++ lib/chiliproject_auto_wiki/patches/enabled_module_patch.rb
@@ -22,13 +22,7 @@ module ChiliprojectAutoWiki
                 source_project = Project.find_by_id(config['project_id'])
                 if source_project
                   source_page = Wiki.find_page(config['wiki_page_name'], :project => source_project)
-                  if source_page
-                    auto_page = project.reload.wiki.pages.new(:title => source_page.title)
-                    auto_page.content = WikiContent.new(:text => source_page.text, :author => User.current)
-                    # Let save fail if there are validation errors: existing page with title, invalid title
-                    auto_page.save
-                  end
-                  
+                  copy_wiki_page(source_page) if source_page
                 end
                 
               end
@@ -44,6 +38,13 @@ module ChiliprojectAutoWiki
       end
 
       module InstanceMethods
+        def copy_wiki_page(source_page)
+          auto_page = project.reload.wiki.pages.new(:title => source_page.title)
+          auto_page.content = WikiContent.new(:text => source_page.text, :author => User.current)
+          # Let save fail if there are validation errors: existing page with title, invalid title
+          auto_page.save
+        end
+
       end
     end
   end
